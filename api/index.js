389b48f872b0658acff6bf9b4d4ee1b7bd83f0d1
Anish Dangol <dangol.anish001@gmail.com>
2024-03-25 21:32:55 +0545

    connected to mongo

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MONGO DB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
