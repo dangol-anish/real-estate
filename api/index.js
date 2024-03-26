ade578b1942c2f6de061024fe4c1fc15771bb419
Anish Dangol <dangol.anish001@gmail.com>
2024-03-26 15:25:15 +0545

    created test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -12,6 +13,8 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
   console.log(`Listening to port 3000!`);
