8c86e313c794bc64e1f020621dffeba06f8b5925
Anish Dangol <dangol.anish001@gmail.com>
2024-03-26 15:47:23 +0545

    added signup

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -14,7 +15,10 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
   console.log(`Listening to port 3000!`);
