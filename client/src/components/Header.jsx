ac57c24bc89bbf605170bbc48a5e455eb3d4a33b
Anish Dangol <dangol.anish001@gmail.com>
2024-03-26 16:09:58 +0545

    created signup page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,19 +28,19 @@ const Header = () => {
         <ul className="flex gap-4 ">
           <Link to="/">
             {" "}
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer font-semibold">
               Home
             </li>
           </Link>
           <Link to="/about">
             {" "}
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer font-semibold">
               About
             </li>
           </Link>
           <Link to="/sign-in">
             {" "}
-            <li className=" text-slate-700 hover:underline cursor-pointer">
+            <li className=" text-slate-700 hover:underline cursor-pointer font-semibold">
               Sign in
             </li>
           </Link>
