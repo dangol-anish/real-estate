a7a36dbed5acacc429ff2e32fb6ee9cf98e84946
Anish Dangol <dangol.anish001@gmail.com>
2024-03-28 17:57:37 +0545

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+// combined all the available reducers here
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// added the basic configuration for the persistence of reducer data in storage (by default: localstorage)
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// persisted all the available reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  // reducer: {
+  //   user: userReducer,
+  // },
+
+  // added all the reducers in one place rather than separate k-v pair for each reducer
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
