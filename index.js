7c8d812bc83ce85c53f0d01c6cc929a15bdc7bf2
Anish Dangol <dangol.anish001@gmail.com>
2024-05-19 00:00:33 +0545

    .

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
