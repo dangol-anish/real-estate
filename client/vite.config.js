5f009b528d098b41232184bfa45842ad29db8639
Anish Dangol <dangol.anish001@gmail.com>
2024-05-18 23:10:03 +0545

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://real-estate-rose-zeta.vercel.app",
         secure: false,
       },
     },
